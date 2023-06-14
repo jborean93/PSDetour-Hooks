98eaa6768a1ee7afc8d4dff7408b51e06ebb76ac
Jordan Borean <jborean93@gmail.com>
2023-06-14 12:17:23 +1000

    Add AcquireCredentialsHandle and improve Secur32 error handling

--- Secur32.ps1
+++ Secur32.ps1
@@ -10,6 +10,27 @@ Function Format-SecBufferDesc {
         1 = 'SECBUFFER_DATA'
         2 = 'SECBUFFER_TOKEN'
         3 = 'SECBUFFER_PKG_PARAMS'
+        4 = 'SECBUFFER_MISSING'
+        5 = 'SECBUFFER_EXTRA'
+        6 = 'SECBUFFER_STREAM_TRAILER'
+        7 = 'SECBUFFER_STREAM_HEADER'
+        8 = 'SECBUFFER_NEGOTIATION_INFO'
+        9 = 'SECBUFFER_PADDING'
+        10 = 'SECBUFFER_STREAM'
+        11 = 'SECBUFFER_MECHLIST'
+        12 = 'SECBUFFER_MECHLIST_SIGNATURE'
+        13 = 'SECBUFFER_TARGET'
+        14 = 'SECBUFFER_CHANNEL_BINDINGS'
+        15 = 'SECBUFFER_CHANGE_PASS_RESPONSE'
+        16 = 'SECBUFFER_TARGET_HOST'
+        17 = 'SECBUFFER_ALERT'
+        18 = 'SECBUFFER_APPLICATION_PROTOCOLS'
+        19 = 'SECBUFFER_SRTP_PROTECTION_PROFILES'
+        20 = 'SECBUFFER_SRTP_MASTER_KEY_IDENTIFIER'
+        21 = 'SECBUFFER_TOKEN_BINDING'
+        22 = 'SECBUFFER_PRESHARED_KEY'
+        23 = 'SECBUFFER_PRESHARED_KEY_IDENTITY'
+        24 = 'SECBUFFER_DTLS_MTU'
     }
 
     if ($BufferDesc -ne [IntPtr]::Zero) {
@@ -43,18 +64,65 @@ Function Format-SecBufferDesc {
             $dataPtr = [System.Runtime.InteropServices.Marshal]::ReadIntPtr($bufferPtr, 8)
             $bufferPtr = [IntPtr]::Add($bufferPtr, 16)
 
-            $data = if ($bufferType -in @(1, 2, 3)) {
-                $bufferBytes = [byte[]]::new($bufferLength)
+            $bufferBytes = [byte[]]::new($bufferLength)
+            if ($dataPtr -and $dataPtr -ne [IntPtr]::Zero) {
                 [System.Runtime.InteropServices.Marshal]::Copy($dataPtr, $bufferBytes, 0, $bufferBytes.Length)
-                [System.Convert]::ToHexString($bufferBytes)
             }
-            else {
-                '0x{0:X8}' -f $dataPtr
-            }
+            $data = [System.Convert]::ToHexString($bufferBytes)
 
             "`t`t[$i] Type: $bufferTypeStr, Length: $bufferLength, Data: $data"
         }
     }
+}
+
+New-PSDetourHook -DllName Secur32.dll -MethodName AcquireCredentialsHandleW {
+    [OutputType([int])]
+    param(
+        [IntPtr]$Principal,
+        [IntPtr]$Package,
+        [int]$CredentialUse,
+        [IntPtr]$LogonId,
+        [IntPtr]$AuthData,
+        [IntPtr]$GetKeyFn,
+        [IntPtr]$GetKeyArgument,
+        [IntPtr]$Credential,
+        [IntPtr]$Expiry
+    )
+
+    <#
+    SECURITY_STATUS SEC_Entry AcquireCredentialsHandle(
+        _In_  SEC_CHAR       *pszPrincipal,
+        _In_  SEC_CHAR       *pszPackage,
+        _In_  ULONG          fCredentialUse,
+        _In_  PLUID          pvLogonID,
+        _In_  PVOID          pAuthData,
+        _In_  SEC_GET_KEY_FN pGetKeyFn,
+        _In_  PVOID          pvGetKeyArgument,
+        _Out_ PCredHandle    phCredential,
+        _Out_ PTimeStamp     ptsExpiry
+    );
+    #>
+
+    $principalStr = ''
+    if ($Principal -ne [IntPtr]::Zero) {
+        $principalStr = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Principal)
+    }
+    $packageStr = ''
+    if ($Package -ne [IntPtr]::Zero) {
+        $packageStr = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Package)
+    }
+
+    $this.State.WriteObject(
+        'AcquireCredentialsHandle(Principal: 0x{0:X8}, Package: 0x{1:X8}, CredentialUse: 0x{2:X8}, LogonId: 0x{3:X8}, AuthData: 0x{4:X8}, GetKeyFn: 0x{5:X8}, GetKeyArgument: 0x{6:X8}, Credential: 0x{7:X8}, Expiry: 0x{8:X8})' -f @(
+        $Principal, $Package, $CredentialUse, $LogonId, $AuthData, $GetKeyFn, $GetKeyArgument,
+        $Credential, $Expiry
+    ))
+    $this.State.WriteObject("`tPrincipal: $principalStr")
+    $this.State.WriteObject("`tPackage: $packageStr")
+
+    $res = $this.Invoke($Principal, $Package, $CredentialUse, $LogonId, $AuthData, $GetKeyFn, $GetKeyArgument,
+        $Credential, $Expiry)
+    $this.State.WriteObject('AcquireCredentialsHandle -> Res: 0x{0:X8}' -f $res)
 }
 
 New-PSDetourHook -DllName Secur32.dll -MethodName InitializeSecurityContextW {
@@ -177,7 +245,10 @@ New-PSDetourHook -DllName Secur32.dll -MethodName DecryptMessage {
 
     $res = $this.Invoke($Context, $Message, $SeqNo, $Qop)
 
-    $qopValue = [System.Runtime.InteropServices.Marshal]::ReadInt32($Qop)
+    $qopValue = 0
+    if ($Qop -ne [IntPtr]::Zero) {
+        $qopValue = [System.Runtime.InteropServices.Marshal]::ReadInt32($Qop)
+    }
 
     $this.State.WriteObject('DecryptMessage -> Res: 0x{0:X8}, Qop: {1}' -f @(
         $res, $qopValue
