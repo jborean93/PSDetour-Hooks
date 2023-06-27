9f08d00c0cc9df0e70646ad5fdf35050e1ccb427
Jordan Borean <jborean93@gmail.com>
2023-06-28 09:38:28 +1000

    Added Get and Set EnvironmentVariable

--- Kernel32.ps1
+++ Kernel32.ps1
@@ -1,3 +1,49 @@
+New-PSDetourHook -DllName Kernel32.dll -MethodName GetEnvironmentVariableW {
+    [OutputType([int])]
+    param(
+        [System.Runtime.InteropServices.MarshalAsAttribute([System.Runtime.InteropServices.UnmanagedType]::LPWStr)]
+        [string]$Name,
+
+        [IntPtr]$Buffer,
+
+        [int]$Size
+    )
+
+    $this.State.WriteObject('GetEnvironmentVariable(Name: ''{0}'', Buffer: 0x{1:X8}, Size: {2})' -f @(
+        $Name, $Buffer, $Size))
+    $res = $this.Invoke($Name, $Buffer, $Size)
+    $err = [System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
+    $this.State.WriteObject('GetEnvironmentVariable - Return: 0x{0:X8}, Size: {1}' -f ($err, $res))
+    if ($res -gt 0) {
+        $envValue = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Buffer, $res)
+        $this.State.WriteObject("`t$envValue")
+    }
+
+    $res
+}
+
+New-PSDetourHook -DllName Kernel32.dll -MethodName SetEnvironmentVariableW {
+    [OutputType([bool])]
+    param(
+        [System.Runtime.InteropServices.MarshalAsAttribute([System.Runtime.InteropServices.UnmanagedType]::LPWStr)]
+        [string]$Name,
+
+        [IntPtr]$Value
+    )
+
+    $this.State.WriteObject('SetEnvironmentVariable(Name: ''{0}'', Value: 0x{1:X8})' -f @(
+        $Name, $Value))
+    if ($Value -ne [IntPtr]::Zero)   {
+        $envValue = [System.Runtime.InteropServices.Marshal]::PtrToStringUni($Value)
+        $this.State.WriteObject("`tValue: $envValue")
+    }
+
+    $res = $this.Invoke($Name, $Value)
+    $this.State.WriteObject('SetEnvironmentVariable - Return: {0}' -f $res)
+
+    $res
+}
+
 New-PSDetourHook -DllName Kernel32.dll -MethodName GetProcessId {
     [OutputType([int])]
     param(
